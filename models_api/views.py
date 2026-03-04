b2b34f138343fe05e4018ec5668382670b5084cb
Marek Zelinka <mzelinka17@gmail.com>
2026-03-04 20:37:38 +0100

    fix types and format

--- models_api/views.py
+++ models_api/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from models_api.models import ModelAuthor
@@ -11,8 +14,9 @@ class ModelAuthorViewSet(viewsets.ModelViewSet):
     serializer_class = ModelAuthorSerializer
 
     @action(detail=True, methods=["get"])
-    def models(self, request, pk=None):
+    def models(self, _request: Request, **kwargs: Any) -> Response:
         author = self.get_object()
         models = author.models_uploaded.all()
         serializer = AIModelSerializer(models, many=True)
+
         return Response(serializer.data)
