10b3f08729599ab22bcd1a962388c125856f7f22
Marek Zelinka <mzelinka17@gmail.com>
2026-03-04 17:42:42 +0100

    add model author api

---
+++ models_api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from models_api.views import ModelAuthorViewSet
+
+router = DefaultRouter()
+router.register(r"authors", ModelAuthorViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
---
+++ models_api/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from models_api.models import ModelAuthor
+from models_api.serializers import AIModelSerializer, ModelAuthorSerializer
+
+
+class ModelAuthorViewSet(viewsets.ModelViewSet):
+    queryset = ModelAuthor.objects.all()
+    serializer_class = ModelAuthorSerializer
+
+    @action(detail=True, methods=["get"])
+    def models(self, request, pk=None):
+        author = self.get_object()
+        models = author.models_uploaded.all()
+        serializer = AIModelSerializer(models, many=True)
+        return Response(serializer.data)
