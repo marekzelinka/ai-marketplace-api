cee9f682e92ae3265cf1d242706cc158f203037b
Marek Zelinka <mzelinka17@gmail.com>
2026-03-04 20:43:34 +0100

    add ai models api

--- models_api/urls.py
+++ models_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from models_api.views import ModelAuthorViewSet
+from models_api.views import AIModelViewSet, ModelAuthorViewSet
 
 router = DefaultRouter()
 router.register(r"authors", ModelAuthorViewSet)
-
+router.register(r"models", AIModelViewSet)
 
 urlpatterns = [path("", include(router.urls))]
--- models_api/views.py
+++ models_api/views.py
@@ -5,8 +5,13 @@ from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from models_api.models import ModelAuthor
-from models_api.serializers import AIModelSerializer, ModelAuthorSerializer
+from models_api.models import AIModel, ModelAuthor
+from models_api.serializers import (
+    AIModelSerializer,
+    ModelAuthorSerializer,
+    ModelBenchmarkSerializer,
+    UsageScenarioSerializer,
+)
 
 
 class ModelAuthorViewSet(viewsets.ModelViewSet):
@@ -18,5 +23,26 @@ class ModelAuthorViewSet(viewsets.ModelViewSet):
         author = self.get_object()
         models = author.models_uploaded.all()
         serializer = AIModelSerializer(models, many=True)
+
+        return Response(serializer.data)
+
+
+class AIModelViewSet(viewsets.ModelViewSet):
+    queryset = AIModel.objects.all()
+    serializer_class = AIModelSerializer
+
+    @action(detail=True, methods=["get"])
+    def usage_scenarios(self, _request: Request, **kwargs: Any) -> Response:
+        model = self.get_object()
+        scenarios = model.usage_scenarios.all()
+        serializer = UsageScenarioSerializer(scenarios, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["get"])
+    def benchmarks(self, _request: Request, **kwargs: Any) -> Response:
+        model = self.get_object()
+        benchmarks = model.benchmarks.all()
+        serializer = ModelBenchmarkSerializer(benchmarks, many=True)
 
         return Response(serializer.data)
